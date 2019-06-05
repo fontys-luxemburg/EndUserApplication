27b28d9d611bafc191742d1e294a0822ae79d090
tjidde-nl <tjidde-nl@users.noreply.github.com>
2019-06-05 12:39:45 +0200

    Added vue app WIP

---
+++ webapp/enduser/src/routes.js
@@ -0,0 +1,12 @@
+import login from "./components/login";
+
+export default routes;
+
+
+const routes = [
+    {
+        path: '/login',
+        name: 'login',
+        component: login
+    },
+];
